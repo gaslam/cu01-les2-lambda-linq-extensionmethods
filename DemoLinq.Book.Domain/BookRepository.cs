8ff15c03e91289ba429a6975cae82f63f9bebd21
mileto1975 <mileto.di.marco@howest.be>
2019-09-22 09:24:31 +0200

    Adds BookRepository class

---
+++ DemoLinq.Book.Domain/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLinq.Book.Domain
+{
+    public class BookRepository
+    {
+        private IEnumerable<Book> Programming;
+
+        private IEnumerable<Book> Romans;
+
+        public IEnumerable<Book> GetProgramming()
+        {
+              Programming = new Book[]
+              {
+                    new Book{BookId=1,Title="C# .Net Core",Uitgever="Prisma"},
+                    new Book{BookId=2,Title="PHP7",Uitgever="Microsoft Publishers"}
+              };
+            return Programming;  
+
+        }
+
+        public IEnumerable<Book> GetRomans()
+        {
+            Romans = new List<Book>()
+            {
+                new Book{BookId=1,Title="De Terminator",Uitgever="Schwarzie"},
+                new Book{BookId=2,Title="Rambo12",Uitgever="Stallone"}
+            };
+            return Romans;
+        }
+
+    }
+}
--- LinqIntro/Program.cs
+++ LinqIntro/Program.cs
@@ -23,50 +23,9 @@ namespace LinqIntro
                 new Book{BookId=1,Title="De Terminator",Uitgever="Schwarzie"},
                 new Book{BookId=2,Title="Rambo12",Uitgever="Stallone"}
             };
-            //filteren zonder Linq
-            //List<Book> booksWithC = new List<Book>();
-
-            /*Book book = new Book();
-            var work = book.DoWork();
-            var books = book.GetBooks();
-            print(books.Result);
-            Console.WriteLine(work.Result);
-            */
-            Worker worker = new Worker();
-            worker.DoWork();
-            Console.Write("Calculating:");
-            while (!worker.WorkDone)
-            {
-                Console.Write("*");
-                Thread.Sleep(100);
-            }
-    }
-
-        static async void  callAsync()
-        {
-            Book book = new Book();
-            var books = await book.GetBooks();
-            print(books);
-        }
-        /*static void print(StringBuilder bookInfo)
-        {
-            Console.WriteLine(bookInfo.ToString());
-        }*/
-        static void print(IEnumerable<Book> books)
-        {
-            foreach(Book book in books)
-            {
-                Console.WriteLine(book.Title);
-            }
+            
         }
 
-        static IEnumerable<Book> FilterBooksWithC(IEnumerable<Book> books)
-        {
-            foreach (var book in books)
-            {
-                if(book.Title.Contains("C"))
-                    yield return book;
-            }
-        }
+        
     }
 }

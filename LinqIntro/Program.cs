a7f836b550084415005c45f7630886020c3fc07c
mileto1975 <mileto.di.marco@howest.be>
2019-09-22 09:25:35 +0200

    Adds cleanup main program

--- LinqIntro/Program.cs
+++ LinqIntro/Program.cs
@@ -12,18 +12,7 @@ namespace LinqIntro
     {
         static void Main(string[] args)
         {
-            IEnumerable<Book> Programming = new Book[]
-             {
-                new Book{BookId=1,Title="C# .Net Core",Uitgever="Prisma"},
-                new Book{BookId=2,Title="PHP7",Uitgever="Microsoft Publishers"}
-            };
-
-            IEnumerable<Book> Romans = new List<Book>()
-            {
-                new Book{BookId=1,Title="De Terminator",Uitgever="Schwarzie"},
-                new Book{BookId=2,Title="Rambo12",Uitgever="Stallone"}
-            };
-            
+            Console.WriteLine("Book repository app");
         }
 
         
